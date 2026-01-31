50dcaa5b3833b357f0415f6120c66fdb0c8a9657
Ravi Tharun <tr565003@gmail.com>
2026-01-31 21:39:32 +0530

    stages

--- src/Apis/Signup.js
+++ src/Apis/Signup.js
@@ -1,16 +1,37 @@
-import { ToastType } from "../ReactToast/Toast"
+import toast, { Toaster } from 'react-hot-toast';
+import axios from "axios"
+import { useNavigate } from 'react-router-dom';
 
-const handelapiSigup = (data, e) => {
+
+const handelapiSigup = async (data, e) => {
   e.preventDefault()
-  console.log(ToastType, ' toastType')
-  console.log(data)
+  try {
+    const response = await axios.post(`http://localhost:5001/api/auth/newDataUser`, { formdata: data })
+    return response
+  } catch (error) {
+
+  }
 }
 const handelLogin = async (data, e) => {
   try {
     e.preventDefault()
-    // e.p
-    console.log(data, "login data")
+    const response = await axios.get(`http://localhost:5001/api/auth/LoginAccount`, {
+      params: {
+        email: data.StudentEmail,
+        Password: data.StudentPassword,
+        role: data.role
+      }
+    })
+    if(response.data.message=="The password is incorrect"){
+      return toast.error("The password is incorrect")
+    }
+    return response
+
+
+    // return response
   } catch (error) {
+    // console.log(error.message)
+    toast.error(error?.message)
 
   }
 }
