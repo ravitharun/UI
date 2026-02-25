1b1dda2de6b14bd3d4b9580eb7e70134b442f01e
Ravi Tharun <tr565003@gmail.com>
2026-02-25 09:35:53 +0530

    fe

--- src/Components/AcctountDeactive.jsx
+++ src/Components/AcctountDeactive.jsx
@@ -11,17 +11,25 @@ import axios from "axios";
 
 function AccountDeactivated() {
     const [poup, setpoup] = useState(false)
-
+    const [issuetype, settype] = useState("")
+    const [priorty, setpriorty] = useState("")
 
 
     const [Reason, setReason] = useState("")
     const handelRequest = async () => {
-        console.log(Header_Token_expry, 'Header_Token_expry')
+        console.log({ name: UserName.name, email: UserName.email, empid: UserName.teacher_Id, Reason, issuetype: issuetype, priorty: priorty }, 'Header_Token_expry')
 
-        if (!Reason) {
+        if (!issuetype || !priorty) {
             toast.error("please enter required Felids")
         }
-        const promise = axios.post("http://localhost:5001/api/Account/UpdateReason", { name: UserName.name, email: UserName.email, empid: UserName.teacher_Id, Reason }, Header_Token_expry)
+        if (issuetype == 'other') {
+
+            if (!Reason) {
+                return toast.error("please enter required Felids")
+
+            }
+        }
+        const promise = axios.post("http://localhost:5001/api/Account/UpdateReason", { name: UserName.name, issuetype, email: UserName.email, empid: UserName.teacher_Id, Reason, priorty }, Header_Token_expry)
         toast.promise(promise, {
 
             loading: "Submitting your request...",
@@ -31,6 +39,15 @@ function AccountDeactivated() {
         setpoup(false)
         setReason("")
     }
+    const issueOptions = [
+        { value: "account_deactivation", label: "Account Deactivation" },
+        { value: "account_reactivation", label: "Account Reactivation Request" },
+        { value: "login_issue", label: "Login Problem" },
+        { value: "profile_issue", label: "Profile Update Issue" },
+        { value: "course_issue", label: "Course Upload Issue" },
+        { value: "technical_bug", label: "Technical Bug" },
+        { value: "other", label: "Other" }
+    ];
     return (
         <>
             <ToastContainer />
@@ -101,6 +118,7 @@ function AccountDeactivated() {
                                 <label className="text-sm font-medium text-gray-700">
                                     Emp name
                                 </label>
+
                                 <input
                                     type="text"
                                     value={UserName.name}
@@ -121,22 +139,51 @@ function AccountDeactivated() {
                                     className={`w-full bg-gray-100 text-gray-600 px-3 py-2 rounded-lg text-sm ${UserName.email && 'cursor-not-allowed'}`}
                                 />
                             </div>
-
-                            {/* Reason */}
-                            <div className="mb-4">
-                                <label className="text-sm font-medium text-gray-700">
-                                    Reason for Reactivation
+                            <div>
+                                <label className="block text-sm font-medium text-gray-700 mb-1">
+                                    Priority
                                 </label>
-                                <textarea
-                                    placeholder="Please explain why your account should be reactivated..."
-                                    required
-                                    rows="3"
-                                    value={Reason}
-                                    className="w-full mt-2 border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
-                                    onChange={(e) => setReason(e.target.value)}
-                                ></textarea>
-                            </div>
+                                <select className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => setpriorty(e.target.value)}>
+                                    <option value="low" selected>select Priority</option>
+                                    <option value="low">Low</option>
+                                    <option value="medium">Medium</option>
+                                    <option value="high">High</option>
+                                </select>
+                            </div><div className="space-y-4">
+                                <div>
+                                    <label className="block text-sm font-medium text-gray-700 mb-1">
+                                        Issue Type
+                                    </label>
+                                    <select className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" onChange={(e) => settype(e.target.value)}>
+                                        <option value="low" selected>select Issue</option>
 
+                                        {issueOptions.map((type, idx) => (
+                                            <option key={type.value} value={type.value}>
+
+                                                {type.label}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                            </div>
+                            {/* Reason */}
+                            {issuetype == 'other'
+
+                                &&
+                                <div className="mb-4">
+                                    <label className="text-sm font-medium text-gray-700">
+                                        Reason for Reactivation
+                                    </label>
+                                    <textarea
+                                        placeholder="Please explain why your account should be reactivated..."
+                                        required
+                                        rows="3"
+                                        value={Reason}
+                                        className="w-full mt-2 border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+                                        onChange={(e) => setReason(e.target.value)}
+                                    ></textarea>
+                                </div>}
+                            <br />
                             {/* Buttons */}
                             <div className="flex justify-end gap-3">
                                 <button
@@ -146,19 +193,19 @@ function AccountDeactivated() {
                                     Cancel
                                 </button>
 
-                                {Reason &&
-                                    <>
 
-                                        <button className="px-4 py-2 text-sm rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition hover:cursor-pointer"
-                                            // disabled={!Reason}
-                                            onClick={handelRequest}
-                                        >
-                                            Send Request
-                                        </button>
-                                    </>
+                                <>
+
+                                    <button className="px-4 py-2 text-sm rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition hover:cursor-pointer"
+                                        // disabled={!Reason}
+                                        onClick={handelRequest}
+                                    >
+                                        Send Request
+                                    </button>
+                                </>
 
 
-                                }
+
 
                             </div>
 
